6ac1f5dd61f567ec47f7a27abeef81f75d8f5ec0
Roberto Abreu <abreuroberto2015@gmail.com>
2021-08-30 08:42:29 -0300

    30/08

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ class ListaConvidados {
         this.convidados = [];
     }
 
-    adicionar(nome, idade, cpf){
-        this.convidados.push({nome: nome, idade: idade, cpf: cpf});
+    adicionar(nome, idade, cpf) {
+        this.convidados.push({ nome: nome, idade: idade, cpf: cpf });
         console.log(this.convidados);
     }
 }
 
 var lista = new ListaConvidados();
 
-document.getElementById("adicionarConvidado").onclick = function(){
+document.getElementById("adicionarConvidado").onclick = function () {
     adicionarConvidado("Roberto", 43, "123456789")
 };
